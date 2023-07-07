f3a81541ab3cb88efce87230df0bfa569e2cd59c
amirjj <amir.jamshidijam@gmail.com>
2023-07-07 22:29:54 +0330

    config changed

--- downloader/config.py
+++ downloader/config.py
@@ -1,1 +1,9 @@
-# config file
+
+LINKS = [
+    'https://ups.music-fa.com/tagdl/1402/',
+    'https://ups.music-fa.com/tagdl/1401/',
+    'https://ups.music-fa.com/tagdl/downloads/',
+    'https://dls.music-fa.com/tagdl/ati/',
+    'https://irsv.upmusics.com/AliBZ/'
+]
+
